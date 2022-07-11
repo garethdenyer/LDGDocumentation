09644e776b7db802fce18a04879401e24571f476
Gareth Denyer <gareth.denyer@sydney.edu.au>
2022-07-11 19:35:32 +1000

    LDG06 Pipette Enhancements
    Improving the functionality of the pipette and visual appearance of the aliquot

--- DataMaker/Assets/Scripts/PipetteAction.cs
+++ DataMaker/Assets/Scripts/PipetteAction.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class PipetteAction : MonoBehaviour
 {
@@ -10,25 +8,57 @@ public class PipetteAction : MonoBehaviour
     public GameObject activeTube;  //set dynamically as pipette sent to a specific tube
     public TMP_InputField setVol;
     public GameObject thecanvas;
-    
+
+    public GameObject aliquot;
+    public float aliquotvol;
+    public TMP_Text info;
+
+    private void Start()
+    {
+        AdjustAliquot();
+    }
+
     public void ThumbAction(string direction)
     {
-        if(activeTube != null)
+        if (activeTube != null)
         {
             if (float.TryParse(setVol.text, out float uL))
             {
-                if(direction == "suckup")  //there may be other options!
+                if (direction == "suckup")  //there may be other options!
                 {
+                    if (uL > activeTube.GetComponentInChildren<LiquidInTube>().volul)
+                    {
+                        uL = activeTube.GetComponentInChildren<LiquidInTube>().volul;
+                    }
                     activeTube.GetComponentInChildren<LiquidInTube>().AdjustVol(-uL);
-                }
-                else
-                {
-                    activeTube.GetComponentInChildren<LiquidInTube>().AdjustVol(uL);
+                    aliquotvol += uL;
                 }
 
-                SendTipToMeniscus();  
+                else if (direction == "dispense")
+                {
+                    if (uL >= aliquotvol)
+                    {
+                        uL = aliquotvol;
+                    }
+                        activeTube.GetComponentInChildren<LiquidInTube>().AdjustVol(uL);
+                        aliquotvol -= uL;
+                }
+
+                AdjustAliquot();
+                SendTipToMeniscus();
             }
         }
+    }
+
+    void AdjustAliquot()
+    {
+        //adjust size and position of aliquot
+        float aliheight = aliquotvol * 2f / 200f;
+        aliquot.transform.localScale = new Vector3(0.3f, aliheight, 0.3f);
+        aliquot.transform.localPosition = new Vector3(0f, aliheight - 2f, 0f);
+
+        //report the volume in the aliquot
+        info.text = aliquotvol.ToString("N0");
     }
 
 
@@ -40,7 +70,7 @@ public class PipetteAction : MonoBehaviour
 
         transform.position = meniscusposn + new Vector3(0f, pipmenoffset, 0f);
 
-        thecanvas.transform.position =  topoftube;
+        thecanvas.transform.position = topoftube + new Vector3(0f, 0f, -1f);  //the minus one keeps the canvas in front of the pipette
     }
 
 
---
+++ DataMaker/Assets/Scripts/PipetteStand.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipetteStand : MonoBehaviour
+{
+    // Attached to PipetteStand
+
+    private void OnMouseDown()
+    {
+        GameObject tip = GameObject.Find("Pipette"); //locate the pipette tip
+
+        //set tipengaged in the previous activeTube to false and now clear the activeTube in PipetteAction 
+        if (tip.GetComponent<PipetteAction>().activeTube != null) //not necessary to do if no activeTube
+        {
+            tip.GetComponent<PipetteAction>().activeTube.GetComponentInChildren<PipetteGuide>().tipengaged = false;  //tip no longer in tube
+            tip.GetComponent<PipetteAction>().activeTube = null;
+        }
+
+        tip.transform.position = transform.position + new Vector3(0f, 2.7f, 0f);  //places pipette just just on stand
+    }
+
+
+}
